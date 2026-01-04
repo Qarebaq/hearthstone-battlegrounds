edf79ed9a5022ef8fe5940008e9942c9c42e1f2e
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-01-05 03:21:29 +0330

    Qarebaq - bindings.cpp upsdated via api system

--- bindings.cpp
+++ bindings.cpp
@@ -1,7 +1,7 @@
 #include <Python.h>
 #include<pybind11/pybind11.h>
 #include<string>
-
+#include "cpp_api.h"
 
 
 namespace py = pybind11;
@@ -12,6 +12,13 @@ std::string hello(){
 }
 
 PYBIND11_MODULE(bgbinding, m) {
-    m.doc() = "test binding for bgbinding";
-    m.def("hello", &hello, "Return a hello string from C++");
+    // m.doc() = "test binding for bgbinding";
+    // m.def("hello", &hello, "Return a hello string from C++");
+    m.doc() = "Battlegrounds C++ bindings (PoC)";
+    m.def("create_match_json", &create_match_json, "Create a match and return match JSON");
+    m.def("push_action_json", &push_action_json, "Push action JSON for a player");
+    m.def("get_state_json", &get_state_json, "Get state JSON for a match");
+    m.def("start_combat_json", &start_combat_json, "Start combat (PoC) and return JSON");
+    m.def("destroy_match", &destroy_match, "Destroy match");
+
 }
