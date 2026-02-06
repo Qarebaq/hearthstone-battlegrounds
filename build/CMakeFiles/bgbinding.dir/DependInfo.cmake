05541d7cb084ebff60bf8680c058468e5c046384
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-02-06 15:51:57 +0330

    Qarebaq - cmake files here

---
+++ build/CMakeFiles/bgbinding.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/amirsmac/Desktop/w/hearthstone-battlegrounds/Board.cpp" "CMakeFiles/bgbinding.dir/Board.cpp.o" "gcc" "CMakeFiles/bgbinding.dir/Board.cpp.o.d"
+  "/Users/amirsmac/Desktop/w/hearthstone-battlegrounds/GameController.cpp" "CMakeFiles/bgbinding.dir/GameController.cpp.o" "gcc" "CMakeFiles/bgbinding.dir/GameController.cpp.o.d"
+  "/Users/amirsmac/Desktop/w/hearthstone-battlegrounds/GameState.cpp" "CMakeFiles/bgbinding.dir/GameState.cpp.o" "gcc" "CMakeFiles/bgbinding.dir/GameState.cpp.o.d"
+  "/Users/amirsmac/Desktop/w/hearthstone-battlegrounds/Shop.cpp" "CMakeFiles/bgbinding.dir/Shop.cpp.o" "gcc" "CMakeFiles/bgbinding.dir/Shop.cpp.o.d"
+  "/Users/amirsmac/Desktop/w/hearthstone-battlegrounds/bindings.cpp" "CMakeFiles/bgbinding.dir/bindings.cpp.o" "gcc" "CMakeFiles/bgbinding.dir/bindings.cpp.o.d"
+  "/Users/amirsmac/Desktop/w/hearthstone-battlegrounds/cpp_api.cpp" "CMakeFiles/bgbinding.dir/cpp_api.cpp.o" "gcc" "CMakeFiles/bgbinding.dir/cpp_api.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/bgbinding.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bgbinding.dir/Board.cpp.o"
+  "CMakeFiles/bgbinding.dir/Board.cpp.o.d"
+  "CMakeFiles/bgbinding.dir/GameController.cpp.o"
+  "CMakeFiles/bgbinding.dir/GameController.cpp.o.d"
+  "CMakeFiles/bgbinding.dir/GameState.cpp.o"
+  "CMakeFiles/bgbinding.dir/GameState.cpp.o.d"
+  "CMakeFiles/bgbinding.dir/Shop.cpp.o"
+  "CMakeFiles/bgbinding.dir/Shop.cpp.o.d"
+  "CMakeFiles/bgbinding.dir/bindings.cpp.o"
+  "CMakeFiles/bgbinding.dir/bindings.cpp.o.d"
+  "CMakeFiles/bgbinding.dir/cpp_api.cpp.o"
+  "CMakeFiles/bgbinding.dir/cpp_api.cpp.o.d"
+  "bgbinding.cpython-314-darwin.so"
+  "bgbinding.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bgbinding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
